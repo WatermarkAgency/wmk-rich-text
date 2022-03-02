24f5f4ba56fd51b9e422b9b829c1657126c0e51e
Peter Washington <peter@wmkagency.com>
2022-03-02 09:17:19 -0700

    minimized ts errors by using contentful's defined types

--- src/classes/RichText.ts
+++ src/classes/RichText.ts
@@ -1,29 +1,24 @@
-import get from "lodash/get";
+import { Block, Document, Inline, Text } from "@contentful/rich-text-types";
 
-export interface RichTextJsonContent {
-  content: {
-    data: {};
-    marks: [];
-    nodeType: string;
-    value: string;
-    content?: {
-      data: {};
-      marks: [];
-      nodeType: string;
-      value: string;
-      content?: {
-        data: {};
-        marks: [];
-        nodeType: string;
-        value: string;
-        content?: [];
-        references?: {};
-      }[];
-    }[];
-  }[];
-  data: {};
-  nodeType: string;
-  references: [];
+export interface RichTextDocument extends Document {
+  references: any[];
+}
+
+export interface RichTextQuery {
+  raw: string;
+  references: any[];
+}
+
+export interface RichTextBlock extends Block {
+  references: object;
+}
+
+export interface RichTextInline extends Inline {
+  references: object;
+}
+
+export interface RichTextText extends Text {
+  references: object;
 }
 
 /**
@@ -37,57 +32,44 @@ export class RichText {
    *
    */
   raw: string;
-  json?: {
-    nodeType: string;
-    data: object;
-    content: RichTextJsonContent[];
-  };
-  references: [];
+  json?: RichTextDocument;
+  references: any[];
 
-  constructor(node: object) {
-    const _refs = get(node, `references`, []);
-    const raw = get(node, `raw`);
-    const json = raw ? JSON.parse(raw) : get(node, `json`);
+  constructor(node: RichTextQuery) {
+    const _refs = node.references ? node.references : [];
+    const raw = node.raw;
+    const json: Document = JSON.parse(raw);
     let refCount = 0;
-    this.raw = get(node, `raw`);
-    this.json = Array.isArray(get(json, `content`))
+    this.raw = raw;
+    this.json = Array.isArray(json.content)
       ? {
           ...json,
-          content: json.content.map(
-            (j: {
-              nodeType: string;
-              data: object;
-              content?: {
-                nodeType: string;
-                data: object;
-                references: {};
-                content: [];
-              }[];
-            }) => {
-              const { nodeType } = j;
-              let r = null;
-              if (nodeType.match(/^embedded/)) {
-                r = _refs[refCount];
-                refCount++;
-              } else if (nodeType.match(/paragraph|heading/)) {
-                j.content.map((pCon) => {
+          content: json.content.map((j) => {
+            const { nodeType } = j;
+            let r = null;
+            if (nodeType.match(/^embedded/)) {
+              r = _refs[refCount];
+              refCount++;
+            } else if (nodeType.match(/paragraph|heading/)) {
+              j.content.map(
+                (pCon: RichTextBlock | RichTextInline | RichTextText) => {
                   const nodeType = pCon.nodeType;
                   if (nodeType.match(/^entry|asset/)) {
                     r = _refs[refCount];
                     refCount++;
                     pCon.references = r;
                   }
-                });
-              } else {
-                console.log(
-                  "The node: " +
-                    nodeType +
-                    " is not yet supported in the RichText class."
-                );
-              }
-              return { ...j, references: r };
+                }
+              );
+            } else {
+              console.log(
+                "The node: " +
+                  nodeType +
+                  " is not yet supported in the RichText class."
+              );
             }
-          )
+            return { ...j, references: r };
+          })
         }
       : undefined;
     this.references = _refs;
@@ -99,7 +81,11 @@ export class RichText {
       content.forEach((text) => {
         const type = text.nodeType;
         if (type === "paragraph") {
-          const innerContent = text.content;
+          const innerContent = text.content as (
+            | RichTextBlock
+            | RichTextInline
+            | RichTextText
+          )[];
           if (Array.isArray(innerContent)) {
             innerContent.forEach((line) => {
               if (line.nodeType === "text") {
