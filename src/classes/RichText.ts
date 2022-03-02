750a895c8d1a3db9e595da72bc7fc21a75d8600b
Peter Washington <peter@wmkagency.com>
2022-03-02 12:50:53 -0700

    hit a dead end as this approach doesn't put reference data in an easily accessible place

--- src/classes/RichText.ts
+++ src/classes/RichText.ts
@@ -1,26 +1,13 @@
-import { Block, Document, Inline, Text } from "@contentful/rich-text-types";
-
-export interface RichTextDocument extends Document {
-  references: any[];
-}
+import { Document } from "@contentful/rich-text-types";
 
 export interface RichTextQuery {
   raw: string;
   references: any[];
 }
 
-export interface RichTextBlock extends Block {
-  references: object;
+export interface ReferenceData {
+  [key: string]: any;
 }
-
-export interface RichTextInline extends Inline {
-  references: object;
-}
-
-export interface RichTextText extends Text {
-  references: object;
-}
-
 /**
  * @class - Reshapes Contentful RichText data into
  * usable JSON for rendering. Also keeps track of
@@ -32,48 +19,66 @@ export class RichText {
    *
    */
   raw: string;
-  json?: RichTextDocument;
+  json?: Document;
   references: any[];
-
   constructor(_node: RichTextQuery) {
     const node = _node ? { ..._node } : undefined;
     const _refs = node?.references ? node.references : [];
     const raw = node?.raw ? node.raw : "";
     const json: Document = raw ? JSON.parse(raw) : undefined;
     let refCount = 0;
-    this.raw = raw;
-    this.json =
-      json && Array.isArray(json.content)
-        ? {
-            ...json,
-            content: json.content.map((j) => {
-              const { nodeType } = j;
-              let r = null;
-              if (nodeType.match(/^embedded/)) {
+    const refactorRefs = (json: Document) => {
+      const refData: ReferenceData = {};
+      if (json && Array.isArray(json.content)) {
+        json.content.forEach((j) => {
+          const { nodeType } = j;
+          let r = null;
+          if (nodeType.match(/^embedded/)) {
+            r = _refs[refCount];
+            refCount++;
+          } else if (nodeType.match(/paragraph|heading/)) {
+            j.content.map((pCon) => {
+              const pNodeType = pCon.nodeType;
+              if (pNodeType.match(/^entry|asset/)) {
                 r = _refs[refCount];
                 refCount++;
-              } else if (nodeType.match(/paragraph|heading/)) {
-                j.content.map(
-                  (pCon: RichTextBlock | RichTextInline | RichTextText) => {
-                    const nodeType = pCon.nodeType;
-                    if (nodeType.match(/^entry|asset/)) {
-                      r = _refs[refCount];
-                      refCount++;
-                      pCon.references = r;
-                    }
-                  }
-                );
-              } else {
-                // console.log(
-                //   "The node: " +
-                //     nodeType +
-                //     " is not yet supported in the RichText class."
-                // );
+                // pCon.references = r;
+                refData[pCon.data.target.sys.id] =
+                  typeof r === "object" && Object.keys(r).length > 0
+                    ? { ...r }
+                    : {
+                        type: "error",
+                        error:
+                          "Query data is missing or this content is archived. ",
+                      };
               }
-              return { ...j, references: r };
-            }),
+            });
+          } else {
+            // console.log(
+            //   "The node: " +
+            //     nodeType +
+            //     " is not yet supported in the RichText class."
+            // );
           }
-        : undefined;
+
+          return { ...j };
+        });
+      } else {
+        return undefined;
+      }
+      const ret =
+        json && Array.isArray(json.content)
+          ? {
+              ...json,
+              references: refData,
+            }
+          : undefined;
+      //console.log(Object.keys(refData).length > 0 && ret);
+      return ret;
+    };
+    this.raw = raw;
+    this.json = refactorRefs(json);
+
     this.references = _refs;
   }
   excerpt = (chars: number = 156): string => {
@@ -83,11 +88,7 @@ export class RichText {
       content.forEach((text) => {
         const type = text.nodeType;
         if (type === "paragraph") {
-          const innerContent = text.content as (
-            | RichTextBlock
-            | RichTextInline
-            | RichTextText
-          )[];
+          const innerContent = text.content;
           if (Array.isArray(innerContent)) {
             innerContent.forEach((line) => {
               if (line.nodeType === "text") {
