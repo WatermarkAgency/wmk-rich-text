866c86219929c6d13d255da0e00eabecdcac0702
Peter Washington <peter@wmkagency.com>
2022-03-04 09:14:55 -0700

    trying a more simplified apporach

--- src/RichTextReact.tsx
+++ src/RichTextReact.tsx
@@ -1,21 +1,13 @@
 import {
   documentToReactComponents,
-  Options,
-  RenderNode
+  NodeRenderer,
+  Options
 } from "@contentful/rich-text-react-renderer";
-import {
-  BLOCKS,
-  INLINES,
-  AssetHyperlink,
-  EntryHyperlink,
-  Table,
-  TableRow,
-  TableHeaderCell,
-  TableCell
-} from "@contentful/rich-text-types";
+import { AssetHyperlink, EntryHyperlink } from "@contentful/rich-text-types";
 import * as React from "react";
 import { RichText, RichTextDocument } from ".";
-import { RichTextReference, RichTextTopLevelBlock } from ".";
+import { RichTextReference } from ".";
+import { RichTextBlock, RichTextInline } from "./classes/RichText";
 
 export interface RichTextAssetHyperlink extends AssetHyperlink {
   reference: RichTextReference;
@@ -25,84 +17,19 @@ export interface RichTextEntryHyperlink extends EntryHyperlink {
   reference: RichTextReference;
 }
 
-export interface RichTextRenderNode extends RenderNode {
-  [BLOCKS.DOCUMENT]: (
-    node: RichTextDocument,
+export interface RichTextNodeRenderer extends NodeRenderer {
+  (
+    node: RichTextBlock | RichTextInline,
     children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.PARAGRAPH]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HEADING_1]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HEADING_2]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HEADING_3]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HEADING_4]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HEADING_5]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HEADING_6]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.EMBEDDED_ENTRY]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.UL_LIST]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.OL_LIST]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.LIST_ITEM]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.QUOTE]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.HR]: () => JSX.Element;
-  [BLOCKS.TABLE]: (node: Table, children: React.ReactNode) => JSX.Element;
-  [BLOCKS.TABLE_ROW]: (
-    node: TableRow,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.TABLE_HEADER_CELL]: (
-    node: TableHeaderCell,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [BLOCKS.TABLE_CELL]: (
-    node: TableCell,
-    children: React.ReactNode
-  ) => JSX.Element;
-  [INLINES.ASSET_HYPERLINK]: (node: RichTextAssetHyperlink) => JSX.Element;
-  [INLINES.ENTRY_HYPERLINK]: (node: RichTextEntryHyperlink) => JSX.Element;
-  [INLINES.EMBEDDED_ENTRY]: (node: RichTextTopLevelBlock) => JSX.Element;
-  [INLINES.HYPERLINK]: (
-    node: RichTextTopLevelBlock,
-    children: React.ReactNode
-  ) => JSX.Element;
+  ): React.ReactNode;
+}
+
+export interface RichTextRenderNode {
+  [k: string]: RichTextNodeRenderer;
 }
 
 export interface RichTextOptions extends Options {
-  renderNode: RichTextRenderNode;
+  renderNode?: RichTextRenderNode;
 }
 
 export const RichTextReact = ({
